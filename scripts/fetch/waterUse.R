2f69cf90d740c5fe25d2847f95fe76ca0ab1cc59
Laura DeCicco <ldecicco@usgs.gov>
2016-11-30 13:13:56 -0600

    Fetching public state data.

---
+++ scripts/fetch/waterUse.R
@@ -0,0 +1,16 @@
+fetch.waterUse <- function(viz){
+  library(dataRetrieval)
+  library(dplyr)
+  library(data.table)
+  
+  all.states <- data.frame()
+  
+  for(i in tolower(state.abb)){
+    state.url <- paste0("http://waterdata.usgs.gov/",i,"/nwis/water_use?format=rdb&rdb_compression=value&wu_area=State Total&wu_year=ALL&wu_category=ALL&wu_category_nms=--ALL Categories--")
+    dater <- importRDB1(state.url, convertType = FALSE)
+    all.states <- bind_rows(all.states, dater)
+  }
+  
+  write.csv(all.states, viz[["location"]], row.names = FALSE)
+  
+}
