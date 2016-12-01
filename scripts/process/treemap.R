e67bf1837d820b6d0e2093fc250b1fa66dc8fa00
Lindsay Carr <lcarr@usgs.gov>
2016-12-01 12:20:20 -0600

    save all treemap list in one giant list

--- scripts/process/treemap.R
+++ scripts/process/treemap.R
@@ -1,21 +1,23 @@
 
-visualize.treemap <- function(viz){
-  ### getting data ready
-  library(dplyr)
+process.treemap <- function(viz){
+  library(dplyr)  
+  library(treemap)
+  
   nationalData_list <- readDepends(viz)[[1]] 
   wateruse_data <- nationalData_list[['nationalData']]%>% 
     mutate(category = as.factor(category),
            color = as.numeric(category))
-  
-  library(treemap)
-  
-  yrs_in_data <- sort(unique(wateruse_data$year))
-  lapply(yrs_in_data, function(y, wateruse_data, viz){
-    wateruse_year <- wateruse_data %>% filter(year == y)
-    filepath_year <- file.path(viz[['location']], paste0(y, '.png'))
-    png(filename=filepath_year)
-    treemap(wateruse_year, index="category", vSize="value", type="index", title=y)
-    dev.off()
-  }, wateruse_data, viz)
 
+  # create treemap for each year, save to list (ignore total category)
+  yrs_in_data <- sort(unique(wateruse_data$year))
+  all_treemaps <- lapply(yrs_in_data, function(y, wu_data, viz){
+    wateruse_year <- wu_data %>% filter(year == y)
+    treemap(wateruse_year, index="category", vSize="value", type="index", title=y)
+  }, wu_data = filter(wateruse_data, category != "Total withdrawals, in Bgal/d"), viz)
+  names(all_treemaps) <- paste0('tm_', yrs_in_data)
+  
+  # add total category to list
+  all_treemaps$total_wu <- filter(wateruse_data, category == "Total withdrawals, in Bgal/d")
+  
+  saveRDS(all_treemaps, file=viz[['location']])
 }
