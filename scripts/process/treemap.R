d93d254d1ba8f720984d2453bc8b8f5ac3726a96
Lindsay Carr <lcarr@usgs.gov>
2016-12-01 11:47:21 -0600

    treemap pkg, not portfolio

--- scripts/process/treemap.R
+++ scripts/process/treemap.R
@@ -7,18 +7,6 @@ visualize.treemap <- function(viz){
     mutate(category = as.factor(category),
            color = as.numeric(category))
   
-  #### trying out different tree maps
-  
-  # library(portfolio)
-  # lapply(yrs_in_data, function(y, wateruse_data){
-  #   wateruse_year <- wateruse_data %>% filter(year == y)
-  #   treemap_1985 <- map.market(id=1:nrow(wateruse_year), area=wateruse_year$value,
-  #                              group=wateruse_year$category, color=wateruse_year$color,
-  #                              main=paste("Wateruse", y))
-  #   filepath_year <- file.path(viz[['location']], paste0(y, '.png'))
-  #   png(filename=filepath_year)
-  # }, wateruse_data)
-  
   library(treemap)
   
   yrs_in_data <- sort(unique(wateruse_data$year))
