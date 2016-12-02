e2211e978362607b09ea6d21408abfd485c6cd19
Jordan S Read <jread@usgs.gov>
2016-12-02 10:38:49 -0600

    updating to do svg shifts

--- scripts/process/process_map.R
+++ scripts/process/process_map.R
@@ -24,6 +24,7 @@ process.state_map <- function(viz){
   # thanks to Bob Rudis (hrbrmstr):
   # https://github.com/hrbrmstr/rd3albers
   
+  # -- if moving any more states, do it here: --
   alaska <- elide(AK, rotate=-50)
   alaska <- elide(alaska, scale=max(apply(bbox(alaska), 1, diff)) / 2.3)
   alaska <- elide(alaska, shift=c(-2100000, -2500000))
@@ -34,14 +35,18 @@ process.state_map <- function(viz){
   hawaii <- elide(hawaii, shift=c(5400000, -1400000))
   row.names(hawaii) <- 'hawaii'
   proj4string(hawaii) <- proj4string(conus)
-  
   states.out <- rbind(conus, alaska, hawaii, makeUniqueIDs = TRUE)
-  out <- list(orig.states = states.out, bbox = bbox(states.out)) # then spatial points for centroids, and other things
   
-  names.st <- names(out)
-  for (name in names.st){
-    sp::plot(scale_transform(out[names(out) == name,], scale.n), add=TRUE, col=cols[col.i], border=NA)
-  }
+  # shifting states to be located at (0,0) in svg-space:
+  shifted.states <- zero_shift_sp(states.out)
+  shifted.centroids <- zero_shift_sp(shifted.states)
+  state.centroids <- state_centroids(states.out) # keeps plot order
+  
+  out <- list(states = states.out, 
+              shifted.states = shifted.states, 
+              shifted.centroids = shifted.centroids,
+              state.centroids = state.centroids, 
+              bbox = bbox(states.out)) # then spatial points for centroids, and other things
   
   saveRDS(out, file = viz[['location']])
 }
@@ -51,4 +56,32 @@ scale_transform <- function(obj, scale){
   centroid <- c(unlist(rgeos::gCentroid(obj,byid=TRUE)@coords))
   obj <- elide(obj, scale=max(apply(bbox(obj), 1, diff)) * scale) # do by area?
   elide(obj, shift=centroid - c(unlist(rgeos::gCentroid(obj,byid=TRUE)@coords)))
+}
+
+state_centroids <- function(sp){
+  state.centroids <- NULL
+  ordered.names <- names(sp)[sp@plotOrder]
+  for (name in ordered.names){
+    obj <- rgeos::gCentroid(sp[names(sp) == name,],byid=TRUE)
+    if (name == ordered.names[1]){
+      state.centroids <- obj
+    } else {
+      state.centroids <- rbind(state.centroids, obj)
+    }
+  }
+  return(state.centroids)
+}
+zero_shift_sp <- function(sp){
+  sp.shifted <- NULL
+  for (name in names(sp)){
+    obj <- sp[names(sp) == name,]
+    centroid <- c(unlist(rgeos::gCentroid(obj,byid=TRUE)@coords))
+    zero.coord <- c(bbox(sp)[1,1], bbox(sp)[2,2]) # min x, but max y
+    if (name == names(sp)[1]){
+      sp.shifted <- elide(obj, shift=zero.coord-centroid)
+    } else {
+      sp.shifted <- rbind(sp.shifted, elide(obj, shift=zero.coord-centroid))
+    }
+  }
+  return(sp.shifted)
 }
