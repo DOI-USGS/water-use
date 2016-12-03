17740dba9dcbdb45126405cddf8a59a1d5cc023d
Jordan Walker <jiwalker@usgs.gov>
2016-12-02 18:24:12 -0600

    Add Total to states

--- scripts/process/scaleStates.R
+++ scripts/process/scaleStates.R
@@ -1,36 +1,36 @@
 #scale states according to water use
-# two different sized states with equal water use should have the 
+# two different sized states with equal water use should have the
 # same area
 library(rgeos)
 library(dplyr)
 
 process.scaleStates <- function(viz){
-  
+
   #read sp, wuClean
   statePoly <- readData(viz[['depends']]$stateMap)$states
   wuClean <- readData(viz[['depends']]$wuClean)
-  
+
   wuClean$state_name <- tolower(wuClean$state_name)
   #get areas, join to wuClean
   areas <- data.frame(gArea(statePoly, byid = TRUE), stringsAsFactors = FALSE)
   areas$state_name <- rownames(areas)
   wuAreas <- left_join(wuClean, areas, by = 'state_name')
   names(wuAreas)[6] <- "area"
-  
+
   #compute ratios
   wuAreas <- mutate(wuAreas, wuPerArea = value/area)
   finalScaleFactors <- data.frame()
   #per category
   for(cat in unique(wuAreas$category)){
     thisCat <- filter(wuAreas, category == cat)
-    
+
     magicState <- thisCat[which.max(thisCat$wuPerArea), ]
     thisCat <- mutate(thisCat, newArea = value * magicState$area / magicState$value)
     thisCat <- mutate(thisCat, scaleFactor = newArea / area)
-    
+
     finalScaleFactors <- bind_rows(finalScaleFactors, thisCat)
   }
-  
+
   #save
   saveRDS(finalScaleFactors, file = viz[['location']])
 }
@@ -42,39 +42,41 @@ library(jsonlite)
 process.scaleFactors2json <- function(viz){
   scaleFactors <- readData(viz[['depends']]$scaleFactors)
   scaleFactorsNational <- readData(viz[['depends']]$scaleFactorsNational)
-  
+
   scaleFactors <- select(scaleFactors, -area, -wuPerArea, -newArea)
-  
+
   #replace spaces with underscore
   scaleFactors <- mutate(scaleFactors, state_name = gsub(" ", "_", scaleFactors$state_name))
-  
-  
+
+
   uniqYears <- unique(scaleFactors$year)
   uniqYears <- uniqYears[order(uniqYears)]
   forJson <- vector("list", length(uniqYears)) #initialize
-  
+
   for(i in 1:length(uniqYears)){
     thisYear <- filter(scaleFactors, year == uniqYears[i])
-    
+
     #convert to list of dfs for each category
     forJson[[i]] <- list(Thermoelectric=filter(thisYear, category == "Thermoelectric"),
                           Industrial = filter(thisYear, category == "Industrial"),
                           Public_Supply = filter(thisYear, category == "Public Supply"),
-                          Irrigation = filter(thisYear, category == "Irrigation"))
+                          Irrigation = filter(thisYear, category == "Irrigation"),
+                          Total = filter(thisYear, category == "Total")
+                         )
   }
   names(forJson) <- uniqYears
-  
+
   natScaleTypes <- list()
-  
+
   for(type in names(scaleFactorsNational)) {
     dat <- scaleFactorsNational[type][[1]]
     uniqYears <- unique(dat$Year)
     natJson <- vector("list", length(uniqYears))
     for(i in 1:length(uniqYears)){
       thisYear <- filter(dat, Year == uniqYears[i])
-      
+
       #convert to list of dfs for each category
-      natJson[[i]] <- list(Thermoelectric=filter(thisYear, category == "Thermoelectric"),
+      natJson[[i]] <- list(Thermoelectric = filter(thisYear, category == "Thermoelectric"),
                            Industrial = filter(thisYear, category == "Industrial"),
                            Public_Supply = filter(thisYear, category == "Public Supply"),
                            Irrigation = filter(thisYear, category == "Irrigation"),
@@ -83,10 +85,10 @@ process.scaleFactors2json <- function(viz){
     names(natJson) <- uniqYears
     natScaleTypes[type][[1]] <- natJson
   }
-  
+
   allJson <- list(totState = forJson, pCapNat = natScaleTypes$pCapData, totNat = natScaleTypes$totData)
-  
+
   jsOut <- toJSON(allJson)
   write(jsOut, viz[['location']])
-   
+
 }
