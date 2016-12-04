67d4d12f8cb655016637b951f968bb3b50bcf71a
Jordan S Read <jread@usgs.gov>
2016-12-03 21:07:15 -0600

    wider hatch for no data

--- scripts/visualize/visualize_map.R
+++ scripts/visualize/visualize_map.R
@@ -50,9 +50,9 @@ visualize.states_svg <- function(viz){
   xml_remove(defs)
   defs <- xml_add_child(svg, 'defs')
   cp <- xml_add_child(defs, 'clipPath', id="svg-bounds")
-  patt <- xml_add_child(defs, 'pattern', id="nodata", patternUnits="userSpaceOnUse", width="6", height="6")
-  xml_add_child(patt, 'rect', height='6',width='6', class='nodata-fill')
-  xml_add_child(patt, 'path', d = "M-1,1 l2,-2 M0,6 l6,-6 M5,7 l2,-2", class='nodata-lines')
+  patt <- xml_add_child(defs, 'pattern', id="nodata", patternUnits="userSpaceOnUse", width="12", height="12")
+  xml_add_child(patt, 'rect', height='12',width='12', class='nodata-fill')
+  xml_add_child(patt, 'path', d = "M-1,1 l2,-2 M0,12 l12,-12 M11,13 l2,-2", class='nodata-lines')
   
   xml_add_child(cp, 'rect', width=vb[3], height=vb[4])
   gb <- xml_add_child(svg, 'g', 'id' = 'state-backgrounds')
